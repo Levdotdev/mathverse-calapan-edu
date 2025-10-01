c3f173bf9def9bdd00eae7c9d5803f36673d8f10
Levdotdev <britolancekianne@gmail.com>
2025-10-01 12:57:54 +0800

    update image

--- app/controllers/CrudController.php
+++ app/controllers/CrudController.php
@@ -106,10 +106,6 @@ class CrudController extends Controller {
 			->encrypt_name();
 
             if(!empty($_FILES["fileToUpload"]["name"]) && $this->upload->do_upload()){
-                if(!empty($char['pic']) && file_exists('uploads/'.$char['pic'])){
-                    unlink('uploads/'.$char['pic']);
-                }
-                
                 $pic = $this->upload->get_filename();
             } else{
                 $pic = $char['pic'];
@@ -131,13 +127,6 @@ class CrudController extends Controller {
 
     function delete($id){
         if($this->lauth->get_role(get_user_id()) == "admin") {
-            $char = $this->CrudModel->find($id);
-
-            if($char){
-                if(!empty($char['pic']) && file_exists('uploads/'.$char['pic'])){
-                    unlink('uploads/'.$char['pic']);
-                }
-            }
             $this->CrudModel->delete($id);
             redirect('trash');
         }
