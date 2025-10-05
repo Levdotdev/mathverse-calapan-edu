9307811e4157042571def6cd05cdf38b3b05a1af
Levdotdev <britolancekianne@gmail.com>
2025-10-05 20:05:11 +0800

    test

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -11,6 +11,8 @@
 	<!-- Styles -->
 	<link href="<?=base_url();?>public/css/main.css" rel="stylesheet">
 	<link href="<?=base_url();?>public/css/style.css" rel="stylesheet">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	<link rel="stylesheet" href="<?= base_url();?>public/css/home.css">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" />
 	<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css" />
 	<link rel="stylesheet" type="text/css"  href="https://cdn.datatables.net/buttons/1.4.0/css/buttons.dataTables.min.css" />
