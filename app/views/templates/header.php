cdc0fd1400274e2303bd35ea4f3229775a9dae51
Levdotdev <britolancekianne@gmail.com>
2025-10-05 20:13:02 +0800

    test3

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -11,7 +11,6 @@
 	<!-- Styles -->
 	<link href="<?=base_url();?>public/css/main.css" rel="stylesheet">
 	<link href="<?=base_url();?>public/css/style.css" rel="stylesheet">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 	<link rel="stylesheet" href="<?= base_url();?>public/css/home.css">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" />
 	<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css" />
