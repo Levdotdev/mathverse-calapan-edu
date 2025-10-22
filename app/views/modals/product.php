20de6d312462c9ee63faa10c998fa902fd268be8
Levdotdev <britolancekianne@gmail.com>
2025-10-23 07:41:38 +0800

    product modal fix

--- app/views/modals/product.php
+++ app/views/modals/product.php
@@ -1,4 +1,4 @@
-<div class="modal fade" id="addProductModal" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false">
+<div class="modal fade" id="addProduct" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false">
   <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
 
