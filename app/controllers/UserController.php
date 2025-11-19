26d777e2a7cc19a4bf493e6164aa164f7aff508a
Levdotdev <britolancekianne@gmail.com>
2025-11-20 07:50:37 +0800

    pos login

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -11,7 +11,6 @@ class UserController extends Controller {
     {
         parent::__construct();
         $this->call->database();
-        $this->call->model('CrudModel');
         if(segment(2) != 'logout') {
             $id = $this->lauth->get_user_id();
             if(!logged_in()){
@@ -29,31 +28,6 @@ class UserController extends Controller {
     }
 
     public function index(){
-        $page = 1;
-        if(isset($_GET['page']) && ! empty($_GET['page'])) {
-            $page = $this->io->get('page');
-        }
-
-        $q = '';
-        if(isset($_GET['q']) && ! empty($_GET['q'])) {
-            $q = trim($this->io->get('q'));
-        }
-
-        $records_per_page = 5;
-
-        $all = $this->CrudModel->page_home($q, $records_per_page, $page);
-        $data['all'] = $all['records'];
-        $total_rows = $all['total_rows'];
-        $this->pagination->set_options([
-            'first_link'     => 'First',
-            'last_link'      => 'Last',
-            'next_link'      => '→',
-            'prev_link'      => '←',
-            'page_delimiter' => '&page='
-        ]);
-        $this->pagination->set_theme('bootstrap'); // or 'tailwind', or 'custom'
-        $this->pagination->initialize($total_rows, $records_per_page, $page,'home-user/?q='.$q);
-        $data['page'] = $this->pagination->paginate();
         $this->call->view('home_user', $data);
     }
 
