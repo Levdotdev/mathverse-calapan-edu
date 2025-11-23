bbec3c8196820da94574cbbbf20165f3a3084608
Levdotdev <britolancekianne@gmail.com>
2025-11-24 07:10:57 +0800

    import csv

--- app/views/modals/inventory_Export.php
+++ app/views/modals/inventory_Export.php
@@ -1,6 +1,6 @@
 <div id="modal-export-confirm" class="modal-overlay hidden">
         <div class="modal-content">
-            <div class="modal-header"><h2>Export Data</h2><button class="modal-close-btn" onclick="closeModal('modal-export-confirm')">&times;</button></div>
+            <div class="modal-header"><h2>Download Data</h2><button class="modal-close-btn" onclick="closeModal('modal-export-confirm')">&times;</button></div>
             <div class="modal-body"><p>Do you want to download the current inventory list as a CSV file in your device?</p></div>
             <div class="modal-footer">
                 <button class="action-btn modal-cancel-btn" onclick="closeModal('modal-export-confirm')">Cancel</button>
