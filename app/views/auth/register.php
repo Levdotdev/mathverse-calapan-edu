b7ab58cf5380b1b517e3a35fb286dc367465b153
Levdotdev <britolancekianne@gmail.com>
2025-11-24 15:04:07 +0800

    last

--- app/views/auth/register.php
+++ app/views/auth/register.php
@@ -8,7 +8,7 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@300;400;600;700&display=swap" rel="stylesheet">
     <!-- Styles -->
     <link href="<?=base_url();?>public/css/main.css" rel="stylesheet">
     <link href="<?=base_url();?>public/css/reset.css" rel="stylesheet">
