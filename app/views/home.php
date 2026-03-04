2986dcda439e7f0590b23581055d76e0e59b02d0
Levdotdev <britolancekianne@gmail.com>
2026-03-04 15:39:47 +0800

    fix

--- app/views/home.php
+++ app/views/home.php
@@ -153,9 +153,8 @@
                                             class="action-icon edit-btn open-product-edit-modal"
                                             title="Edit Question"
                                             data-id="<?= $question['id']; ?>"
-                                            data-name="<?= $question['name']; ?>"
-                                            data-category="<?= $question['category']; ?>"
-                                            data-price="<?= $question['price']; ?>">
+                                            data-name="<?= $question['fName']; ?>"
+                                            data-category="<?= $question['type']; ?>"
                                             <i class="fas fa-pen"></i>
                                         </button>
                                         <button title="Delete Product" data-id="<?= $question['id']; ?>" class="action-icon delete-btn open-product-delete-modal"><i class="fas fa-trash"></i></button>
