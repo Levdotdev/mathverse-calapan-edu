cafa3e3bfc9e0ca10db10f69023780a1b7d4c6a7
Levdotdev <britolancekianne@gmail.com>
2026-03-06 15:15:35 +0800

    null

--- app/views/home.php
+++ app/views/home.php
@@ -4,8 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <<<<<<< HEAD
+<<<<<<< HEAD
     <title>Admin | MathVerse</title>
 =======
+=======
+>>>>>>> parent of c2ead57 (design)
     <title>Teacher Dashboard | MathVerse</title>
     
 >>>>>>> parent of c2ead57 (design)
